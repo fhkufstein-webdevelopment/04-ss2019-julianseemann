9e758ce12742e70ed0ec432a97b10b1bcdace813
seemanjulian <1810653188@stud.fh-kufstein.ac.at>
2019-04-08 17:16:31 +0200

    Aufgabe 2

--- js/user.js
+++ js/user.js
@@ -17,30 +17,35 @@ $(document).ready(function() {
             return false;
         }
 
-        //@todo
-        //1. get values
-        //2. create a new element
-        //3. somehow add them to userListBody
-        //4. update number of current users
-        //5. clear entries from the form
-        //6. maybe do something else... :-)
+        var username_element = document.getElementById('username');
+        var username = username_element.value;
+        userListBody.push(username);
+        var currentLength = userListBody.length;
 
-        //your code follows here
+        var table = document.getElementById('table');
+        var row = table.insertRow(-1);
 
+
+
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+
+        cell1.innerHTML = currentLength;
+        cell2.innerHTML = username;
+        cell3.innerHTML = "<button type=\"button\" class=\"btn btn-secondary btn-danger deleteTrigger\" title=\"Löschen\"><i class=\"fa fa-trash\"></i></button>";
+
+        userZaehler ++;
         return false;
-    });
+        });
+
+        $(userListBody).on('click', '.deleteTrigger', function(){
+        $(this).closest('tr').remove();
+
+        });
 
 
-    $('.deleteTrigger').click(function() {
-        //@todo
-        //1. remove current user from dom
-        //2. update number of current users
+        });
 
-        //your code follows here
-
-
-    });
-
-    //maybe some code follows here
 
 });
